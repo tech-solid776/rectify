249ea14b33fdacccf7209a3591d7bd8affdc530f
James-Colli <nwakaezeonyedikajames@gmail.com>
2025-03-14 15:43:50 +0100

    Initial commit

---
+++ recti-site/src/layout/WalletLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const WalletLayout = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  )
+}
+
+export default WalletLayout
